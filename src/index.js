81d03140cfee3001610d03544ae80039a95c1d04
lewypopescu <popesculewy@gmail.com>
2024-08-09 10:03:10 +0200

    ok routing

--- src/index.js
+++ src/index.js
@@ -6,12 +6,9 @@ import { Provider } from "react-redux";
 import App from "./components/App";
 import { store } from "./store/store";
 
-// const basename =
-//   process.env.NODE_ENV === "production" ? "/footballgo-app-react" : "";
-
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <HashRouter basename={"/footballgo-app-react"}>
+    <HashRouter basename="/footballgo-app-react">
       <Provider store={store}>
         <App />
       </Provider>
